44d39b9d110a0d111892a1aff9183eab08d7f794
_irijon_ <103136574+nojirilucas@users.noreply.github.com>
2025-05-11 15:43:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,87 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import React, { useEffect } from "react";
+
+export default function App() 
+{
+  const requestUserPermission = async () => 
+  {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) 
+    {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+
+  useEffect(() => 
+    {
+    if (requestUserPermission()) 
+    {
+      messaging()
+        .getToken()
+        .then((token) => 
+        {
+          console.log(token);
+        });
+    } 
+    
+    else 
+    {
+      console.log("Permission not granted", authStatus);
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => 
+      {
+        if (remoteMessage) 
+        {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+        }
+      });
+
+    messaging().onNotificationOpenedApp((remoteMessage) => 
+    {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage.notification
+      );
+    });
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => 
+    {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => 
+    {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Gerenciador de Notificações</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: 
+  {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
